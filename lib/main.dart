ae2bc35740dc1baa85c4b8e9bbf711c0944a5429
memo figueredo <memof80@gmail.com>
2020-11-07 20:26:51 -0500

    widget container

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('hello'),
+          ),
+        ),
+      ),
+    );
+  }
 }
